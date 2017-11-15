07f6fb4417127c96388c6e7586fd8904af3e4683
Guillermo Orellana <wiyarmir@gmail.com>
2017-11-15 01:39:37 +0000

    more cosmetics. also why do I need an application class at all. wrong name, wrong place.

--- app/src/main/java/es/guillermoorellana/tags/PusherTestApplication.kt
+++
@@ -1,9 +0,0 @@
-package es.guillermoorellana.tags
-
-import android.app.Application
-
-class PusherTestApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
